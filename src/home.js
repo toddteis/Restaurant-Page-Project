debcd7e13e09164a1e6c33a0bc7053e2aa0c4e67
toddt <toddteis@gmail.com>
2022-06-27 06:39:44 +1000

    Added html & content generation to home.js.

--- src/home.js
+++ src/home.js
@@ -2,7 +2,24 @@ const home = (() => {
 
   function createHome() {
     const element = document.createElement('div');
-    element.textContent = 'from inside home';
+    element.classList.add('text-shade-container');
+    const firstDivEle = document.createElement('div');
+    const secDivEle = document.createElement('div');
+    firstDivEle.classList.add('shade');
+    secDivEle.classList.add('text');
+
+    const h1Ele = document.createElement('h1');
+    const sectionEle = document.createElement('section');
+    h1Ele.innerHTML = "<span>Z</span>" + "eus\'s" + " " + "<span>K</span>" + "itchen";
+    secDivEle.append(h1Ele);
+    
+    sectionEle.textContent = 'An authentic Greek restaurant offering an eating experience like no other.';
+    sectionEle.classList.add('main-home');
+    secDivEle.append(sectionEle);
+
+    element.appendChild(firstDivEle);
+    element.appendChild(secDivEle);
+
     return element
   }
 
