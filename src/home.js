964304886016f9712aa5a4facabe665fc215012d
toddt <toddteis@gmail.com>
2022-06-28 07:20:50 +1000

    Changed html generation in home.js.

--- src/home.js
+++ src/home.js
@@ -9,13 +9,17 @@ const home = (() => {
     secDivEle.classList.add('text');
 
     const h1Ele = document.createElement('h1');
-    const sectionEle = document.createElement('section');
+    // const sectionEle = document.createElement('section');
+    const paraEle1 = document.createElement('p');
+    const paraEle2 = document.createElement('p');
+    // sectionEle.classList.add('main-home');
     h1Ele.innerHTML = "<span>Z</span>" + "eus\'s" + " " + "<span>K</span>" + "itchen";
     secDivEle.append(h1Ele);
     
-    sectionEle.textContent = 'An authentic Greek restaurant offering an eating experience like no other.';
-    sectionEle.classList.add('main-home');
-    secDivEle.append(sectionEle);
+    paraEle1.textContent = 'An authentic Greek restaurant offering an eating experience like no other.';
+    paraEle2.textContent = 'Dine in, pick up or deliver.';
+    secDivEle.append(paraEle1);
+    secDivEle.append(paraEle2);
 
     element.appendChild(firstDivEle);
     element.appendChild(secDivEle);
