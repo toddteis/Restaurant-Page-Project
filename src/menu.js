65f28d5a1c389c6010dfcb056efbfc15e6d930a6
toddt <toddteis@gmail.com>
2022-07-01 13:57:19 +1000

    Added menu items with text & images.

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,12 @@
+import ImageGrilledChicken from './grilledchicken.jpg';
+import GyrosWrap from './gyroswrap.jpg';
+import FoodPlatter from './foodplatter.jpg';
+import MeatballPlatter from './meatballplatter.jpg';
+import Souvlaki from './souvlaki.jpg';
+import FigSalad from './figsalad.jpg';
+import EggPlant from './grilledeggplant.jpg';
+import GrilledFish from './grilledfish.jpg';
+
 const menu = (() => {
 
   function createMenu() {
@@ -11,55 +20,54 @@ const menu = (() => {
     element.appendChild(firstDivEle);
     element.appendChild(secDivEle);
     
-    // const menuItems = [];
     const menuItems = [
       {
-        title: 'item1',
-        filePath: './xxx.png',
-        description: 'A brief description about the dish',
-        price: '20.00' 
+        title: 'Grilled Chicken Skewers',
+        filePath: ImageGrilledChicken,
+        description: 'Marinated grilled chicken skewers.',
+        price: '32.00' 
       },
       {
-        title: 'item2',
-        filePath: './xxx.png',
-        description: 'A brief description about the dish',
-        price: '20.00' 
+        title: 'Gyros Wrap',
+        filePath: GyrosWrap,
+        description: 'Gyros wrapped in a pita bread',
+        price: '25.00' 
       },
       {
-        title: 'item3',
-        filePath: './xxx.png',
-        description: 'A brief description about the dish',
-        price: '20.00' 
+        title: 'Assorted Food Platter',
+        filePath: FoodPlatter,
+        description: 'Assorted platter with souvlaki, rice & pita',
+        price: '28.00' 
       },
       {
-        title: 'item4',
-        filePath: './xxx.png',
-        description: 'A brief description about the dish',
-        price: '20.00' 
+        title: 'Lamb Meatball Platter',
+        filePath: MeatballPlatter,
+        description: '100% Lamb meatball platter',
+        price: '26.00' 
       },
       {
-        title: 'item5',
-        filePath: './xxx.png',
-        description: 'A brief description about the dish',
-        price: '20.00' 
+        title: 'Souvlaki',
+        filePath: Souvlaki,
+        description: 'Souvlaki with fried potatoes, tzatziki dip & pita',
+        price: '29.00' 
       },
       {
-        title: 'item6',
-        filePath: './xxx.png',
-        description: 'A brief description about the dish',
-        price: '20.00' 
+        title: 'Arugula Fig Salad',
+        filePath: FigSalad,
+        description: 'Arugula Fig Salad with Walnuts and Feta Cheese',
+        price: '24.00' 
       },
       {
-        title: 'item7',
-        filePath: './xxx.png',
-        description: 'A brief description about the dish',
-        price: '20.00' 
+        title: 'Grilled Eggplant',
+        filePath: EggPlant,
+        description: 'Grilled Eggplant stuffed with couscous, tomatoes, cilantro with ...',
+        price: '27.00' 
       },
       {
-        title: 'item8',
-        filePath: './xxx.png',
-        description: 'A brief description about the dish',
-        price: '20.00' 
+        title: 'Grilled Fish',
+        filePath: GrilledFish,
+        description: 'Grilled Fish with Squid & Totatoes served with Tzatziki Sauce',
+        price: '23.00' 
       }
     ];
 
@@ -79,9 +87,13 @@ const menu = (() => {
       h3Ele.textContent = menuItems[i].title;
       cardEle.append(h3Ele);
       // Image
-      const imgEle = document.createElement('img');
-      imgEle.setAttribute('src', menuItems[i].filePath);
+      // const imgEle = document.createElement('img');
+      // imgEle.setAttribute('src', menuItems[i].filePath);
+      // imgEle.classList.add('card-img');
+      // cardEle.append(imgEle);
+      const imgEle = new Image();
       imgEle.classList.add('card-img');
+      imgEle.src = menuItems[i].filePath;
       cardEle.append(imgEle);
       // Footer
       const cardFooterEle = document.createElement('div');
