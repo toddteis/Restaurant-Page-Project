490e20f7ae58808a5845767360fd418e84c628ff
toddt <toddteis@gmail.com>
2022-07-04 16:26:13 +1000

    Added social icons and styled to contacts page and content to the Open Hours, phone and address sub headings.

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,7 @@
+import Facebook from './logo-facebook.svg';
+import Twitter from './logo-twitter.svg';
+import Instagram from './logo-instagram.svg';
+
 const contact = (() => {
 
   function createContact() {
@@ -7,7 +11,6 @@ const contact = (() => {
     const secDivEle = document.createElement('div');
     firstDivEle.classList.add('shade');
     secDivEle.classList.add('contact-text-container');
-    // secDivEle.id = 'contact-text';
     element.appendChild(firstDivEle);
     element.appendChild(secDivEle);
     // heading
@@ -21,6 +24,26 @@ const contact = (() => {
     // Address, socials & message
     const textEle = document.createElement('div');
     textEle.classList.add('contact-text');
+    // Open hours
+    const openHoursEle = document.createElement('div');
+    const openHoursHeadEle = document.createElement('h4');
+    openHoursHeadEle.textContent = 'Open Hours';
+    openHoursEle.append(openHoursHeadEle);
+    const openHoursParaEle = document.createElement('p');
+    openHoursParaEle.textContent = '11:00am till late.';
+    openHoursEle.append(openHoursParaEle);
+    textEle.append(openHoursEle);
+    // Phone
+    const phoneEle = document.createElement('div');
+    phoneEle.classList.add('phone-address');
+    const phoneHeadEle = document.createElement('h4');
+    phoneHeadEle.textContent = 'Phone';
+    phoneEle.append(phoneHeadEle);
+    const phoneParaEle = document.createElement('p');
+    phoneParaEle.textContent = '05 5555 5555';
+    phoneEle.append(phoneParaEle);
+    textEle.append(phoneEle);
+
     // address
     const addressEle = document.createElement('div');
     addressEle.classList.add('contact-address');
@@ -28,17 +51,35 @@ const contact = (() => {
     addressHeadEle.textContent = "Address";
     addressEle.append(addressHeadEle);
     const addressParaEle = document.createElement('p');
-    addressParaEle.textContent = '1111 Ocean Street Maroochydore Qld';
+    addressParaEle.textContent = '5555 Ocean Street Maroochydore Qld';
     addressEle.append(addressParaEle);
     textEle.append(addressEle);
-    // socials
+    // Socials
     const socialsEle = document.createElement('div');
     socialsEle.classList.add('contact-socials');
     const socialsHeadEle = document.createElement('h4');
     socialsHeadEle.textContent = 'Socials';
+    socialsEle.classList.add('socials');
     socialsEle.append(socialsHeadEle);
+    const socialsParaEle = document.createElement('p');
+    // Facebook
+    const faceBookEle = document.createElement('img');
+    faceBookEle.setAttribute('src', Facebook);
+    faceBookEle.classList.add('social-icon');
+    socialsParaEle.append(faceBookEle);
+    // Twitter
+    const twitterEle = document.createElement('img');
+    twitterEle.setAttribute('src', Twitter);
+    twitterEle.classList.add('social-icon')
+    socialsParaEle.append(twitterEle);
+    // Instagram
+    const instagramEle = document.createElement('img');
+    instagramEle.setAttribute('src', Instagram);
+    instagramEle.classList.add('social-icon');
+    socialsParaEle.append(instagramEle);
+    socialsEle.append(socialsParaEle);
     textEle.append(socialsEle);
-    // message
+    // Message
     const messageEle = document.createElement('div');
     messageEle.classList.add('contact-message');
     const messageHeadEle = document.createElement('h4');
@@ -46,7 +87,7 @@ const contact = (() => {
     messageEle.append(messageHeadEle);
     textEle.append(messageEle);
     detailsEle.append(textEle);
-    // map
+    // Map
     const mapEle = document.createElement('div');
     mapEle.classList.add('contact-map');
     const mapHeadEle = document.createElement('h4');
